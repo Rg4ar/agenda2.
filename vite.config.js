15316c8fe1f77ee02e11be4a795b03242d097d58
Alber Rod <funteamrd@gmail.com>
2026-07-07 20:19:36 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,3 +13,10 @@ export default defineConfig({
   plugins: [react()],
   base: '/agenda2/'
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/agenda2/'
+})
